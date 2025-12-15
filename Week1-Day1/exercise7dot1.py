60f3502cae28f605ad4d2b62c74c7bbca89f0e5d
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2025-12-15 11:17:26 +0200

    update

--- Week1-Day1/exercise7dot1.py
+++ Week1-Day1/exercise7dot1.py
@@ -3,14 +3,14 @@
 # If the number is a divisible by five, print Buzz.
 # If the number is a divisible by both three and five, print FizzBuzz instead.
 
-a = int(input("Give me a number between 1 and 100 "))
+num = int(input("Give me a number between 1 and 100 "))
 
 #if a >= 1 and a <= 100:
-if a/3:
-    print("Fizz")
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
 
-elif a/5:
+elif num % 5 == 0:
     print("Buzz")
 
-elif a/3 and a/5:
-    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
