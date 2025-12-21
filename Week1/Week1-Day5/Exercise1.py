9092180ce5ecc55f492a970dd883fbe8be9c615f
Philip Josolowitz <philipjosolowitz@Philips-MacBook-Pro.local>
2025-12-21 20:30:53 +0200

    update

--- Week1/Week1-Day5/Exercise1.py
+++ Week1/Week1-Day5/Exercise1.py
@@ -17,22 +17,22 @@
 # print (history)
 
 # Dictionaries and Lists
-shirts = [
-  {
-    'name': 'Awesome T-shirt 3000',
-    'size': 'S',
-    'price': 20
-  },
-  {
-    'name': 'Awesome T-shirt 3000',
-    'size': 'M',
-    'price': 25
-  },
-  {
-    'name': 'Awesome T-shirt 3000',
-    'size': 'L',
-    'price': 30
-  },
-]
-for shirt in shirts:
-  print(shirt['size'])
+# shirts = [
+#   {
+#     'name': 'Awesome T-shirt 3000',
+#     'size': 'S',
+#     'price': 20
+#   },
+#   {
+#     'name': 'Awesome T-shirt 3000',
+#     'size': 'M',
+#     'price': 25
+#   },
+#   {
+#     'name': 'Awesome T-shirt 3000',
+#     'size': 'L',
+#     'price': 30
+#   },
+# ]
+# for shirt in shirts:
+#   print(shirt['size'])
