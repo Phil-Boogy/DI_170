39c6739f6b4eb67e69f4f6b7e797730de4cbd69b
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2025-12-30 09:25:13 +0200

    update

---
+++ Week2/Day3/tictactoe_Sandbox.py
@@ -0,0 +1,96 @@
+# o_move = ["", ""]
+# x_move = ["", ""]
+
+# function that takes players move AND validates it
+# def o_player_move():
+#     column_move = int(input ("Tell me which column number you want: "))
+#     row_move = int(input ("Tell me which row number you want: "))
+#     if spaces [column_move-1] [row_move-1] == " ":
+#         spaces [column_move-1] [row_move-1] = "O"
+#     board()
+
+# player = ["X", "O"]
+# turn = 0
+# if turn % 2 == 0:
+#     player = player [0]
+# else:
+#     player = player [1]
+# turn +=1
+spaces = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+def whose_turn (turn):
+    players = ["X", "O"]
+    if turn % 2 == 0:
+        return players [0]
+    else:
+        return players [1]
+    
+# Funtion that takes players move only
+def player_move(player):
+    row_move = int(input (f"Player {player}, tell me which row number you want: "))
+    column_move = int(input (f"Player {player}, tell me which column number you want: "))
+    return row_move, column_move
+
+# function that validates players move only
+def move_validator(player):
+    row, col = player_move (player)
+    if spaces [row-1] [col-1] != " ":
+        print("That space is taken!")
+        row, col = player_move (player)
+    else:    
+        spaces [row-1] [col-1] = player
+
+# def x_player_move():
+#     column_move = int(input ("Tell me which column number you want: "))
+#     row_move = int(input ("Tell me which row number you want: "))
+#     if spaces [column_move-1] [row_move-1] == " ":
+#         spaces [column_move-1] [row_move-1] = "X"
+#     board()
+
+def board ():
+    print("**************")
+    print(f"{spaces[0][0]} | {spaces[0][1]} | {spaces[0][2]}")
+    print("--------------")
+    print(f"{spaces[1][0]} | {spaces[1][1]} | {spaces[1][2]}")
+    print("--------------")
+    print(f"{spaces[2][0]} | {spaces[2][1]} | {spaces[2][2]}")
+    print("**************")
+
+def is_winner (player):
+        #check rows
+    if (spaces[0][0] == spaces[0][1] == spaces[0][2] == player or
+        spaces[1][0] == spaces[1][1] == spaces[1][2] == player or
+        spaces[2][0] == spaces[2][1] == spaces[2][2] == player or
+       # check columns
+        spaces[0][0] == spaces[1][0] == spaces[2][0] == player or
+        spaces[0][1] == spaces[1][1] == spaces[2][1] == player or
+        spaces[0][2] == spaces[1][2] == spaces[2][2] == player or
+        # check diagonal
+        spaces[0][0] == spaces[1][1] == spaces[2][2] == player or
+        spaces[0][2] == spaces[1][1] == spaces[2][0] == player):
+        print (f"Player {player} wins!")
+        return True 
+    # check if tied
+    if " " not in spaces [0] and " " not in spaces [1] and " " not in spaces [2]:
+        print ("tie game") 
+        return True
+    return False
+         
+def game_play ():
+    turn = 0
+    game_on = True
+
+    while game_on:
+        board ()
+        player = whose_turn (turn)
+        move_validator(player)
+        if is_winner(player):
+            game_on = False
+        turn+=1
+
+# def ready_for_school (child):
+#     if breakfast_finished = false:
+#         eat
+#     else:
+#         brush teeth
+
+game_play ()
