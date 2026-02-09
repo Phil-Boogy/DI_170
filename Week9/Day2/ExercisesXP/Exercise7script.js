d86d77336e6095cda907af2636ca0fafc4d8e109
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2026-02-09 21:14:49 +0200

    bunch of exercises

---
+++ Week9/Day2/ExercisesXP/Exercise7script.js
@@ -0,0 +1,16 @@
+// Exercise 7 : Welcome
+// Instructions
+// John has just signed in to your website and you want to welcome him.
+// Create a Navbar in your HTML file.
+// In your js file, create a self invoking funtion that takes 1 argument: the name of the user that just signed in.
+// The function should add a div in the nabvar, displaying the name of the user and his profile picture.
+(function (userName) {
+    const navbar = document.getElementById('navbar');
+    const userDiv = document.createElement('div');
+    userDiv.className = 'user-info';
+    userDiv.innerHTML = `
+        <img src="https://heronscrossing.vet/wp-content/uploads/Golden-Retriever-2048x1365.jpg" alt="${userName}">
+        <span>Welcome, ${userName}!</span>
+    `;
+    navbar.appendChild(userDiv);
+})('John');
