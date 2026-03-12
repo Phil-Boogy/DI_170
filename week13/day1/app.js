37ade8b422d638994b391ac22b059f6ed8926452
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2026-03-12 17:46:30 +0200

    whatevs

---
+++ week13/day1/app.js
@@ -0,0 +1,9 @@
+//console.log('hello world')
+
+//fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+import { add, subtract, mult, div } from "./math/math.js"
+
+console.log(add(6, 7));
