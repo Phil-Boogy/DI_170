86b86963f2f84d91357ccb86f92a15fd83c2d21e
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2026-03-15 20:25:49 +0200

    more

--- week13/day3/server.js
+++ week13/day3/server.js
@@ -28,9 +28,11 @@ app.get("/user/:a/:b", (req, res) => {
 });
 
 app.post("/user", (req, res) => {
-    console.lof(req.body)
-    res.json(users)
-})
+    const { name, email } = req.body;
+    const newUser = { name, email, id: users.length + 1 };
+    users.push(newUser);
+    res.json({ msg: 'new user created' });
+});
 
 app.put("/user/:userid", (req, res) => {
     const { name, email } = req.body
@@ -43,6 +45,17 @@ app.put("/user/:userid", (req, res) => {
     users[index] = { ...users[index], name, email };
     res.json(users);
 
+})
+
+app.delete('/user/:userid', (req, res) => {
+    const { userid } = req.params;
+    const index = users.findIndex((item) => item.id == userid);
+    if (index === -1) {
+        res.status(404).json({ msg: 'user to modify not found' })
+        return
+    }
+    users.splice(index, 1);
+    res.json(users);
 })
 // query syntax ?key=value, ?firstname=philip
 
