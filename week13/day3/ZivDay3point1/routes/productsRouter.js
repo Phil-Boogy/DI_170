61d4ed4b67678880b3ae2cc7bba2f1c54f6d89c7
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2026-03-23 18:39:35 +0200

    remove ziv files from tracking

--- week13/day3/ZivDay3point1/routes/productsRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getAllProducts } = require("../controllers/productsController.js");
-const router = Router();
-
-router.get("/", getAllProducts);
-
-module.exports = {
-  productsRouter: router,
-};
